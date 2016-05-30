9034901208d4dce33f3d7e9da2527931a39311a1
Cliff Lau <cliffdlau@gmail.com>
2016-05-30 12:55:39 -0400

    Removed all boilerplate methods.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,4 @@
 import { combineReducers } from 'redux';
-import user from 'reducers/user';
-import topic from 'reducers/topic';
-import message from 'reducers/message';
 
 import garden from 'reducers/garden';
 import drawer from 'reducers/drawer';
@@ -12,9 +9,6 @@ import { routerReducer as routing } from 'react-router-redux';
 // Combine reducers with routeReducer which keeps track of
 // router state
 const rootReducer = combineReducers({
-  user,
-  topic,
-  message,
   garden,
   plant,
   drawer,
--- app/routes.jsx
+++ app/routes.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from 'containers/App';
-import Vote from 'containers/Vote';
-import About from 'containers/About';
-import LoginOrRegister from 'containers/LoginOrRegister';
-import Dashboard from 'containers/Dashboard';
-
 import Garden from 'containers/Garden';
 
 /*
@@ -15,33 +10,9 @@ import Garden from 'containers/Garden';
  * state from the store after it has been authenticated.
  */
 export default (store) => {
-  const requireAuth = (nextState, replace, callback) => {
-    const { user: { authenticated }} = store.getState();
-    if (!authenticated) {
-      replace({
-        pathname: '/login',
-        state: { nextPathname: nextState.location.pathname }
-      });
-    }
-    callback();
-  };
-
-  const redirectAuth = (nextState, replace, callback) => {
-    const { user: { authenticated }} = store.getState();
-    if (authenticated) {
-      replace({
-        pathname: '/'
-      });
-    }
-    callback();
-  };
   return (
     <Route path="/" component={App}>
-      <IndexRoute component={Vote} />
-      <Route path="login" component={LoginOrRegister} onEnter={redirectAuth} />
-      <Route path="dashboard" component={Dashboard} onEnter={requireAuth} />
-      <Route path="about" component={About} />
-      <Route path="garden" component={Garden} />
+      <IndexRoute component={Garden} />
     </Route>
   );
 };
